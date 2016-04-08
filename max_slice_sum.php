4747df47db5f6dd8c652c899fb2bc4ec31d339a2
bagart <bagart@list.ru>
2016-04-08 21:40:38 +0300

    clear for text

--- max_slice_sum.php
+++ max_slice_sum.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Suppose you have an array of integers, both positive and negative, in no particular order.  Find the largest possible sum of any continuous subarray.  For example, if you have all positive numbers, the largest sum would be the sum of the whole array; if you have all negative numbers, the largest sum is 0 (the null subarray)
+ * mixe slice sum from array
  */
 require_once __DIR__ . '/test.inc.php';
 
--- search_sorted_array.php
+++ search_sorted_array.php
@@ -1,7 +1,7 @@
 <?php
 /**
   * @doc https://ru.wikipedia.org/wiki/%D0%94%D0%B2%D0%BE%D0%B8%D1%87%D0%BD%D1%8B%D0%B9_%D0%BF%D0%BE%D0%B8%D1%81%D0%BA
-  * Write a listbox-style binary search for an ordered array of integers.  Listbox-style means that you should return the index of the first item greater than or equal to the item being searched for; if all items are less, you should return the index of the last item.  You are guaranteed that there is at least one item in the array.
+  * 
  */
 require_once __DIR__ . '/test.inc.php';
 
