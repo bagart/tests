576f617dcc14ff91d396af77613e6d6c44c5c047
BAGArt <baltaev.artur@gmail.com>
2017-07-17 01:10:03 +0300

    fix buffer in rain2

--- php/rain_fill/rain_fill.php
+++ php/rain_fill/rain_fill.php
@@ -46,6 +46,7 @@ if ($unit_tests) {
 foreach (
     [
         [100000, 100],
+        [10000, 10000],
         [100, 100000]
     ]
     as $params) {
@@ -79,12 +80,83 @@ foreach (
         $times[] = ($t2 - $t1);
     }
     echo "avg time diff range2 - range1:", round(array_sum($times) / count($times),
-        2), "sec (LAST: ", round(100 * end($times) / $t1, 2), "%)\n";
+        2), "sec (LAST diff: ", round(100 * end($times) / $t1, 2), "%)\n";
     echo "first mem diff range2 - range1: ", round(($m2 - $m1) / 1024 / 1024,
         2), "mb (", round(100 * ($m2 - $m1) / min($m2, $m1), 2), "%)\n";
 }
 
 function rain1($data)
+{
+    global $debug;
+    $left = -1;//first idx-1 by x
+    $right = count($data); //last+1 idx by x
+    $result = 0;
+    $waterline = 0;// min($left, $right)
+    $left_mem = [];//left mem by y
+    $right_mem = [];//right mem by y
+    $left_max = 0;//left max
+    $right_max = 0;//right max
+    //move from different side |-> .... <-|
+    while (++$left <= --$right) {
+        $waterline_cur = max(
+            $waterline,
+            min($data[$left], $data[$right]),
+            min($data[$left], $right_max),
+            min($data[$right], $left_max)
+        );
+
+        $result += ($waterline_cur > $data[$left] ? $waterline_cur - $data[$left] : 0)
+            + ($right != $left && $waterline_cur > $data[$right] ? $waterline_cur - $data[$right] : 0);
+
+        //just pre-calc
+        $water_left = max($data[$left], $waterline_cur) + 1;
+        $water_right = max($data[$right], $waterline_cur) + 1;
+
+        for ($y = $waterline + 1; $y < $water_left; ++$y) {
+            $result += ($left_mem[$y] ?? 0);
+            unset($left_mem[$y]);
+        }
+        for ($y = $waterline + 1; $y < $water_right; ++$y) {
+            $result += ($right_mem[$y] ?? 0);
+            unset($right_mem[$y]);
+        }
+
+        for ($y = $water_left; $y <= $left_max; ++$y) {
+            $left_mem[$y] = ($left_mem[$y] ?? 0) + 1;
+        }
+
+        for ($y = $water_right; $y <= $right_max; ++$y) {
+            $right_mem[$y] = ($right_mem[$y] ?? 0) + 1;
+        }
+
+        if ($debug) {
+            echo json_encode([
+                'result' => '>>> ' . $result . ' <<<',
+                'left' => [
+                    $left => $data[$left],
+                    'max' => $left_max,
+                    'mem' => $left_mem,
+                ],
+                'right' => [
+                    $right => $data[$right],
+                    'max' => $right_max,
+                    'mem' => $right_mem,
+                ],
+                'waterline' => $waterline,
+                'waterline_cur' => $waterline_cur,
+            ], JSON_PRETTY_PRINT), "\n";
+        }
+
+        $left_max = max($left_max, $data[$left]);
+        $right_max = max($right_max, $data[$right]);
+        $waterline = $waterline_cur;
+
+    }
+
+    return $result;
+}
+
+function rain2($data)
 {
     global $debug;
     $idx = count($data);
@@ -119,71 +191,3 @@ function rain1($data)
 
     return $result;
 }
-
-
-function rain2($data)
-{
-    global $debug;
-    $left = -1;//first idx-1 by x
-    $right = count($data); //last+1 idx by x
-    $result = 0;
-    $waterline = 0;// min($left, $right)
-    $left_mem = [];//left mem by y
-    $right_mem = [];//right mem by y
-    $left_max = 0;//left max
-    $right_max = 0;//right max
-    //move from different side |-> .... <-|
-    while (++$left <= --$right) {
-        $waterline_cur = max(
-            $waterline,
-            min($data[$left], $data[$right]),
-            min($data[$left], $right_max),
-            min($data[$right], $left_max)
-        );
-
-        $result += ($waterline_cur > $data[$left] ? $waterline_cur - $data[$left] : 0)
-            + ($right != $left && $waterline_cur > $data[$right] ? $waterline_cur - $data[$right] : 0);
-
-        for ($y = $waterline + 1; $y <= max($data[$left], $waterline_cur); ++$y) {
-            $result += ($left_mem[$y] ?? 0);
-            unset($left_mem[$y]);
-        }
-        for ($y = $waterline + 1; $y <= max($data[$right], $waterline_cur); ++$y) {
-            $result += ($right_mem[$y] ?? 0);
-            unset($right_mem[$y]);
-        }
-
-        for ($y = $data[$left] + 1; $y <= $left_max; ++$y) {
-            $left_mem[$y] = ($left_mem[$y] ?? 0) + 1;
-        }
-
-        for ($y = $data[$right] + 1; $y <= $right_max; ++$y) {
-            $right_mem[$y] = ($right_mem[$y] ?? 0) + 1;
-        }
-
-        if ($debug) {
-            echo json_encode([
-                'result' => '>>> ' . $result . ' <<<',
-                'left' => [
-                    $left => $data[$left],
-                    'max' => $left_max,
-                    'mem' => $left_mem,
-                ],
-                'right' => [
-                    $right => $data[$right],
-                    'max' => $right_max,
-                    'mem' => $right_mem,
-                ],
-                'waterline' => $waterline,
-                'waterline_cur' => $waterline_cur,
-            ], JSON_PRETTY_PRINT), "\n";
-        }
-
-        $left_max = max($left_max, $data[$left]);
-        $right_max = max($right_max, $data[$right]);
-        $waterline = $waterline_cur;
-    }
-
-    return $result;
-}
-
--- php/rain_fill/readme.md
+++ php/rain_fill/readme.md
@@ -1,13 +1,14 @@
 ```
-$ php php/rain_fill/rain_fill.php
+avg time diff range2 - range1:0.42sec (LAST diff: 811.66%)
+first mem diff range2 - range1: 0.01mb (0.15%)
 
-10 test for 100000 elements with rand(0, 100)
-avg time diff range2 - range1:-0.19sec (LAST: -40.49%)
-first mem diff range2 - range1: 0.01mb (0.12%)
+10 test for 10000 elements with rand(0, 10000)
+avg time diff range2 - range1:4.31sec (LAST diff: 38982.99%)
+first mem diff range2 - range1: 0mb (0%)
 
 10 test for 100 elements with rand(0, 100000)
-avg time diff range2 - range1:-0.17sec (LAST: -40.01%)
-first mem diff range2 - range1: 6mb (57.69%)
+avg time diff range2 - range1:0.37sec (LAST diff: 2555%)
+first mem diff range2 - range1: 3.99mb (62.28%)
 
 ```
    
